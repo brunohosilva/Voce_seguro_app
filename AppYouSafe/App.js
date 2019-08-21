417f34fc8e65ea1bbedb9afce42b3d19a6830bf8
Bruno Oliveira <bruno.oliveira@climatempo.com>
2019-08-20 23:34:34 -0300

    Changed to react native expo, and displayed to user car theft locations

--- AppYouSafe/App.js
+++ AppYouSafe/App.js
@@ -1,42 +1,99 @@
-import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
+import React, { Component } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import MapView from 'react-native-maps';
+
 
 export default class App extends Component {
+  state = {
+    location: null,
+    errorMessage: null,
+  };
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+    console.log(location.coords.latitude)
+    console.log(location.coords.longitude)
+  };
 
   render() {
-    return (
-      <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        >
+    let latitude = 0;
+    let longitude = 0;
 
-          <MapView.Marker
-            coordinate={{
-              latitude: 37.78825,
-              longitude: -122.4324
-            }}
-            title={"title"}
-            description={"description"}
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      latitude = parseFloat(this.state.location.coords.latitude);
+      longitude = parseFloat(this.state.location.coords.longitude);
+    }
+
+    let locations = {
+      markers: [{
+        title: 'Houve um assalto',
+        coordinates: {
+          latitude: -23.175993,
+          longitude: -45.8565098
+        },
+      },
+      {
+        title: 'Houve um assalto',
+        coordinates: {
+          latitude: -23.1745971,
+          longitude: -45.8547501
+        },  
+      }]
+    }
+
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        region={{
+          latitude: parseFloat(latitude),
+          longitude: parseFloat(longitude),
+          latitudeDelta: 0.009,
+          longitudeDelta: 0.009,
+        }}
+      >
+
+        <MapView.Marker
+          coordinate={{
+            latitude: latitude,
+            longitude: longitude
+          }}
+          title={"Minha Localização"}
+          description={"Estou aqui"}
+        />
+
+      {locations.markers.map(marker => (
+          <MapView.Marker 
+            coordinate={marker.coordinates}
+            title={marker.title}
+            image={require('./icon/stolecarmin.png')}
           />
+        ))}
       </MapView>
-    </View >
     );
   }
 }
+
