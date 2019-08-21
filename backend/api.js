6c6ce4e7a414b7b559dc26f6f722931bd918959c
Bruno Oliveira <bruno.oliveira@climatempo.com>
2019-08-20 23:38:47 -0300

    Adjusted array format to right format

--- backend/api.js
+++ backend/api.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var app = express();
 
-const latlon = [
-  { latitude: 49.986111, longitude: 20.061667 },
-  { latitude: 50.193139, longitude: 20.288717 },
-  { latitude: 49.740278, longitude: 19.588611 },
-  { latitude: 50.061389, longitude: 19.938333 },
-  { latitude: 50.174722, longitude: 20.986389 },
-  { latitude: 50.064507, longitude: 19.920777 },
-];
+const latlon =[{
+  title: 'Houve um assalto aqui!!',
+  coordinates: {
+    latitude: -23.175993,
+    longitude: -45.8565098
+  },
+},
+{
+  title: 'Houve um assalto aqui!!',
+  coordinates: {
+    latitude: -23.1745971,
+    longitude: -45.8547501
+  },  
+}]
 
 app.get('/latlon', function (req, res) {
   res.send(latlon);
@@ -16,4 +22,5 @@ app.get('/latlon', function (req, res) {
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+  console.log(latlon)
 });
