5363579f8997a5eeb0b0f5c2bc1e0876e17c763d
Mayra Bernardes <mayra.bernardesc@outlook.com>
2019-08-30 09:32:17 -0300

    Add dependencies

--- backend/api.js
+++ backend/api.js
@@ -9,6 +9,7 @@ fs.createReadStream('./data/data.csv')
   .pipe(csv({separator: ';'}))
   .on('data', (data) => results.push(data))
   .on('end', () => {
+    console.log(results)
     results.map((x) => {
       const latlon = [{
         "title": "Assalto",
