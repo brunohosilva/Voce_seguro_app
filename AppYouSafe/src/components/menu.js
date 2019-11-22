2ee6b1a3ea56531cf50b14b3a3d8069706ee1884
Iago <iagocardial@tecsus.com.br>
2019-11-22 09:06:37 -0300

    acrescentando toggle icon na nav bar

--- AppYouSafe/src/components/menu.js
+++ AppYouSafe/src/components/menu.js
@@ -6,12 +6,21 @@ import {
   View,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import ToggleSwitch from 'toggle-switch-react-native'
 
 export default class MenuSideBar extends Component {
   render() {
     return (
       <View style={[styles.container, { backgroundColor: '#fff' }]}>
         <Text> <Icon name="rocket" size={30} color="#900" />Conteúdo side bar</Text>
+        <ToggleSwitch
+          onColor="green"
+          offColor="grey"
+          label="Furto de Roubo"
+          labelStyle={{ color: "black", fontWeight: "900" }}
+          size="large"
+          onToggle={isOn => console.log("changed to : ", isOn)}
+        />
       </View>
     );
   }
